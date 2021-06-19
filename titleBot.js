06586d75e0e7dc7ee7bab097be7e46c0039bbc55
Isla <79656307+DogCatPuppyLover@users.noreply.github.com>
2021-06-19 14:30:18 -0700

    Create titleBot.js

---
+++ titleBot.js
@@ -0,0 +1,10 @@
+setInterval(title, 5000);
+function title() {
+$.getJSON("https://api.scratch.mit.edu/studios/5842709/comments/", function(data) {
+    $.ajax({
+        type: "PUT",
+        url: "https://scratch.mit.edu/site-api/galleries/all/5842709/",
+        data: JSON.stringify({"title":"♥️ LGBTQ+ ♥️ " + data[0].content}) 
+    });
+});
+}
