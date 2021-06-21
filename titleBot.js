27acc6ee8c66d621514dbe62214a2608bb1c5371
Isla <79656307+DogCatPuppyLover@users.noreply.github.com>
2021-06-20 19:51:53 -0700

    Update titleBot.js
    Added console.log

--- titleBot.js
+++ titleBot.js
@@ -1,10 +1,11 @@
 setInterval(title, 5000);
 function title() {
-$.getJSON("https://api.scratch.mit.edu/studios/5842709/comments/", function(data) {
-    $.ajax({
-        type: "PUT",
-        url: "https://scratch.mit.edu/site-api/galleries/all/5842709/",
-        data: JSON.stringify({"title":"♥️ LGBTQ+ ♥️ " + data[0].content}) 
+    $.getJSON("https://api.scratch.mit.edu/studios/5842709/comments/", function(data) {
+        $.ajax({
+            type: "PUT",
+            url: "https://scratch.mit.edu/site-api/galleries/all/5842709/",
+            data: JSON.stringify({"title":"♥️ LGBTQ+ ♥️ " + data[0].content}) 
+        });
+        console.log("♥️ LGBTQ+ ♥️ " + data[0].content);
     });
-});
 }
