44998304998faaec15059be5974ae893927528de
Isla <79656307+DogCatPuppyLover@users.noreply.github.com>
2021-06-25 18:12:13 -0700

    Update titleBot.js

--- titleBot.js
+++ titleBot.js
@@ -3,7 +3,7 @@ function title() {
     $.getJSON("https://api.scratch.mit.edu/studios/29963772/comments/", function(data) {
         $.ajax({
             type: "PUT",
-            url: "https://scratch.mit.edu/site-api/galleries/all/5842709/",
+            url: "https://scratch.mit.edu/site-api/galleries/all/29963772/",
             data: JSON.stringify({"title":"♥️ LGBTQ+ ♥️ " + data[0].content}) 
         });
         console.log("♥️ LGBTQ+ ♥️ " + data[0].content);
