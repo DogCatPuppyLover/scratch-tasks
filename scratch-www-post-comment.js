233f215960200665214e0cb9dd6a2ef288fb5133
Isla <79656307+DogCatPuppyLover@users.noreply.github.com>
2021-07-08 07:44:27 -0700

    Update scratch-www-post-comment.js

--- scratch-www-post-comment.js
+++ scratch-www-post-comment.js
@@ -1,4 +1,4 @@
-async function postData(url = 'https://api.scratch.mit.edu/proxy/comments/studio/5842709', data = {}) {
+async function postData(url = 'https://api.scratch.mit.edu/proxy/comments/studio/29963772', data = {}) {
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify({"content":"test","parent_id":"","commentee_id":""})
